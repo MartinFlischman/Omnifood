f5a23f3743718d01f4ec01fc1a2c1afb2568893a
Martin Flischman <mflischman@icloud.com>
2021-12-04 06:53:39 +0200

    Media queries, HTML and JavaScript
    - Z-index set on the mobile nav button,
    - Anchor links set for the page sections,
    - Event listener added for the mobile nav button when it is pressed/clicked,
    - Smooth scrolling added to the body selector.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-console.log("Hello World!");
+///////////////////////////////////////////////////////////
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEL = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+	headerEL.classList.toggle("nav-open");
+});
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
